91113022680839e6583e1b5385bbce17efbc8887
miguelmerchan22 <miguelmerchan@hotmail.fr>
2021-08-20 15:44:24 -0500

    update

--- src/cons.js
+++ src/cons.js
@@ -4,6 +4,6 @@ const PRE = "https://precio-site.herokuapp.com/api/v1/servicio/precio/v2/SITE";
 
 const WS = "T9yD14Nj9j7xAB4dbGeiX9h8unkKHxuWwb";//T9yD14Nj9j7xAB4dbGeiX9h8unkKHxuWwb recibe los huerfanos por defecto
 
-const SC = "TPd9Xhoas9rWYsihNJFuShNnNbqd3vYbsV";// direccion del contrato
+const SC = "TPwoKHR6W1gzDEksmmePHyVdZG9TtewKmE";// direccion del contrato
 
 export default {proxy, WS,  SC, PRE};
